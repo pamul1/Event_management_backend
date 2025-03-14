bab0a892c0d6b80319556733dd7a5d97a1a4a7b2
pamul1 <109163253+pamul1@users.noreply.github.com>
2025-03-13 19:39:52 -0500

    Commit2

--- Routes/attendanceRoutes.js
+++ Routes/attendanceRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
 export const attendance = express()
 attendance.use(express.json())
-import { getAttendance, postAttendance } from '../Controllers/attendanceControllers.js'
+import { getAttendance, postAttendance, deleteAttendance } from '../Controllers/attendanceControllers.js'
 
 attendance.get('/:event_id', getAttendance)
 attendance.post('/', postAttendance)
+attendance.attendance('/', deleteAttendance)
